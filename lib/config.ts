3444cfbdc08eb969547b24120615594745ccb2c2
Sharif Mohammad Eunus <sharif.eunus@gmail.com>
2022-09-13 16:27:13 +0600

    Full notion

---
+++ lib/config.ts
@@ -0,0 +1,5 @@
+// TODO: change this to the notion ID of the page you want to test
+export const rootNotionPageId = 'e8e65e9364a34e5eade37e2aae0f00c7'
+
+export const isDev =
+  process.env.NODE_ENV === 'development' || !process.env.NODE_ENV
