60d7866e0ed5052d502bcb02a05bfaf4b34187b1
Sharif Mohammad Eunus <sharif.eunus@gmail.com>
2022-10-08 19:53:57 +0600

    font change

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,8 +4,9 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang='en'>
-        <Head> 
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet"/>
+        <Head>
+        <link href="https://fonts.googleapis.com/css2?family=Figtree:wght@400;600&display=swap" rel="stylesheet" />
+
         </Head>
         <body>
           <Main />
