5cf46427d9398d951fde4c236fa1d36d59835fa7
Sharif Mohammad Eunus <sharif.eunus@gmail.com>
2024-09-29 22:56:55 +0600

    wip

--- .eleventy.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(eleventyConfig) {
-	eleventyConfig.addPassthroughCopy("main.css");
-  // eleventyConfig.addCollection("posts", function(collectionApi) {
-  //   // Return all items in the collection
-  //   return collectionApi.getFilteredByGlob("./posts/*.md");
-  // });
-
-  };
